ea0b57243b9352710ac4eb01fe9ba3f6aa7541b5
yu-so-young2 <sy980911@gmail.com>
2024-08-25 03:32:18 +0900

    launch, runBlocking, delay 간단한 예제

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,21 @@
 package ssoyoung
 
+import kotlinx.coroutines.*
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    /**
+     * launch : Coroutine Builder. 코드의 나머지 부분과 동시에 새로운 코루틴을 실행하여 독립적으로 작동함
+     * delay : 특별한 suspend 함수. 특정 시간동안 코루틴을 일시 중단함. 일시 중단하면 기본 스레드는 차단되지 않기 때문에 다른 코루틴에서 스레드를 이어서 사용할 수 있음.
+     * runBlocking : Coroutine Builder. 코루틴은 모두 runBlocking 안에 포함되어 있지 않으면 에러가 발생
+     */
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    fun main() = runBlocking { // this: CoroutineScope
+        launch { // launch a new coroutine and continue
+            delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+            println("World!") // print after delay
+        }
+        println("Hello") // main coroutine continues while a previous one is delayed
     }
 }
